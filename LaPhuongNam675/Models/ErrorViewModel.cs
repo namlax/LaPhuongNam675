084690b30ce9b41e663ecada38f833182df2f8fd
Lã Nam <namla2000k@gmail.com>
2021-10-06 07:10:40 +0700

    First_commit_LPN675

---
+++ LaPhuongNam675/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LaPhuongNam675.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
